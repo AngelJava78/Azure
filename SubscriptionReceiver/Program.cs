31fa3f473b87b29c7f0f4fbd5b5c92cb19c5b2cb
Angel Javier <angel_java@hotmail.com>
2021-11-01 22:33:11 -0600

    Azure Service Bus Sender and Receiver was added to project to read themes from Azure Service Bus

---
+++ SubscriptionReceiver/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace SubscriptionReceiver
+{
+    class Program
+    {
+        static string subscriptionName = "subscription1";
+        static string connectionString = "Endpoint=sb://servicebusajv.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=XeKFtxeKTpqLvJDW4T2rYjHnESuYNZL9PRfRMmTa2LU=";
+        static string topicName = "myservicebustheme";
+        static ServiceBusClient client;
+        static ServiceBusProcessor processor;
+        static async Task Main()
+        {
+            // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+            // of the application, which is best practice when messages are being published or read
+            // regularly.
+            //
+            // Create the clients that we'll use for sending and processing messages.
+            client = new ServiceBusClient(connectionString);
+
+            // create a processor that we can use to process the messages
+            var options = new ServiceBusProcessorOptions
+            {
+                AutoCompleteMessages = false
+            };
+            processor = client.CreateProcessor(topicName, subscriptionName, options);
+
+            try
+            {
+                // add handler to process messages
+                processor.ProcessMessageAsync += MessageHandler;
+
+                // add handler to process any errors
+                processor.ProcessErrorAsync += ErrorHandler;
+
+                // start processing 
+                await processor.StartProcessingAsync();
+
+                Console.WriteLine("Wait for a minute and then press any key to end the processing");
+                Console.ReadKey();
+
+                // stop processing 
+                Console.WriteLine("\nStopping the receiver...");
+                await processor.StopProcessingAsync();
+                Console.WriteLine("Stopped receiving messages");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await processor.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body} from subscription: {subscriptionName}");
+
+            // complete the message. messages is deleted from the subscription. 
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+    }
+}
---
+++ TopicSender/Program.cs
@@ -0,0 +1,58 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TopicSender
+{
+    class Program
+    {
+
+
+        public static async Task Main(string[] args)
+        {
+            var connectionString = "Endpoint=sb://servicebusajv.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=XeKFtxeKTpqLvJDW4T2rYjHnESuYNZL9PRfRMmTa2LU=";
+            var topicName = "myservicebustheme";
+            var client = new ServiceBusClient(connectionString);
+            var sender = client.CreateSender(topicName);
+
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            var products = new List<Product>
+            {
+                new Product { Id = 11, Name="TRE", Operation="New"},
+                new Product { Id = 12, Name ="TVDE", Operation ="Get"},
+                new Product { Id = 14, Name ="Vestimenta", Operation="Update"},
+                new Product { Id = 18, Name="Regalo", Operation="Delete"}
+            };
+
+            foreach (var product in products)
+            {
+
+                var newMessage = JsonSerializer.Serialize(product);
+                var message = new ServiceBusMessage(newMessage)
+                {
+                    ContentType = "application/json",
+                };
+                Console.WriteLine(newMessage);
+                if (!messageBatch.TryAddMessage(message))
+                {
+                    throw new Exception($"The message {newMessage} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {products.Count} messages has been published to the topic.");
+            }
+            finally
+            {
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
